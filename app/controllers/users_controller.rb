b0738b8a1be6be187b932d511cdca461a1a41175
Alan Ruiz <gheryk90@gmail.com>
2020-03-19 12:41:15 +0100

    home view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,39 @@
 class UsersController < ApplicationController
+  before_action :ser_user
+
+  def home
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    # if @user.update()
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email, :city)
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'users#home'
+
+  resources :users do
+    resources :works
+  end
 end
