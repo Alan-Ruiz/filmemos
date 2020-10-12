b61dbc5ac1c5604c56016525a15d763d21b2c60e
Alan Rodriguez <gheryk90@gmail.com>
2020-10-12 13:15:34 +0200

    UsersHelpers in apli-controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
+  include UsersHelpers
 
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
