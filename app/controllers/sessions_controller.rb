81aa02ee8224d3a3b3fb59571b3e9aa96acee499
Alan Rodriguez <gheryk90@gmail.com>
2020-10-12 13:24:34 +0200

    session controller for logging in and out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+  def new
+    if !logged_in?
+      @user = User.new
+      render :new
+    end
+
+  def create
+    @user = User.find_by(username:login_params[:username])
+    if @user && @user.authenticate(login_params[:password])
+      session[:user_id] = @user.user_id
+      redirect_to root_path
+    else
+      @error = "Invalid username or password"
+      render = :new
+    end
+
+  def destroy
+    session.destroy
+    redirect_to root_path
+  end
+
+private
+  def login_params
+    params.require(:user).permit(:username, :password)
+  end
+end
